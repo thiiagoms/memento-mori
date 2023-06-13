fb11788e7467417d1988097ed86fa8f86bcf3d0d
thiiagoms <thiagom.devsec@gmail.com>
2023-06-13 07:07:54 -0300

    feat: create memento cli printer :sparkles:
    Create a simple CLI Printer helper and enum for colors in CLI

---
+++ src/Enums/Color.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MementoMori\Enums;
+
+/**
+ * Colors package
+ *
+ * @package MementoMori\Tools
+ * @author Thiago <thiiagoms@proton.me>
+ * @version 1.1
+ */
+abstract class Color
+{
+    public const RED = "\e[31m";
+
+    public const YELLOW = "\e[33m";
+
+    public const GREEN = "\e[32m";
+}
